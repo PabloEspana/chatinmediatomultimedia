ac1092a5325c2623b264c40a59167f9eb0381a1a
PabloEspana <pablo_cesar1995@hotmail.com>
2018-12-02 23:56:12 -0500

    Envío de mensajes entre dos dispositivos aún con errores y sin mostrar en interfaz (solo en toast)

--- app/src/main/java/juanmanuelco/facci/com/soschat/BLUETOOTH/Activities/ChatActivity.java
+++ app/src/main/java/juanmanuelco/facci/com/soschat/BLUETOOTH/Activities/ChatActivity.java
@@ -1,16 +1,23 @@
 package juanmanuelco.facci.com.soschat.BLUETOOTH.Activities;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.database.DataSetObserver;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import juanmanuelco.facci.com.soschat.BLUETOOTH.Controllers.ChatController;
 import juanmanuelco.facci.com.soschat.BLUETOOTH.Entities.ChatMessage;
 import juanmanuelco.facci.com.soschat.R;
 import juanmanuelco.facci.com.soschat.BLUETOOTH.Adapters.ChatArrayAdapter;
@@ -20,18 +27,38 @@ public class ChatActivity extends AppCompatActivity {
     private EditText edit;
     private ChatArrayAdapter chatArrayAdapter;
     private ListView listView;
-    private EditText chatText;
-    private Button buttonSend;
+    private EditText textoMensaje; // texto a enviar
+    private Button botonEnviar;  // 
     private boolean lado = false;
+
+    public BluetoothAdapter bluetoothAdapter;  // adaptador bluetooth
 
     Intent i;
     String nombre_destino;
     String direccion_destino;
 
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_OBJECT = 4;
+    public static final int MESSAGE_TOAST = 5;
+    public static final String DEVICE_OBJECT = "device_name";
+
+    private static final int REQUEST_ENABLE_BLUETOOTH = 1;
+    private ChatController chatController;
+    private BluetoothDevice connectingDevice;
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bt_activity_chat);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth no disponible!", Toast.LENGTH_SHORT).show();
+            finish();
+        }
         findByIds();
         configurarActividad();
 
@@ -39,20 +66,20 @@ public class ChatActivity extends AppCompatActivity {
         chatArrayAdapter = new ChatArrayAdapter(getApplicationContext(), R.layout.bt_element_right_msg);
         listView.setAdapter(chatArrayAdapter);
 
-        chatText.setOnKeyListener(new View.OnKeyListener() {
+        /*textoMensaje.setOnKeyListener(new View.OnKeyListener() {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
                     return sendChatMessage();
                 }
                 return false;
             }
-        });
+        });*/
 
         // Evento click al botón enviar
-        buttonSend.setOnClickListener(new View.OnClickListener() {
+        botonEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                sendChatMessage();
+                sendChatMessage(textoMensaje.getText().toString());
             }
         });
 
@@ -71,11 +98,94 @@ public class ChatActivity extends AppCompatActivity {
 
     }
 
-    private boolean sendChatMessage(){
-        chatArrayAdapter.add(new ChatMessage(lado, chatText.getText().toString()));
-        chatText.setText("");
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BLUETOOTH);
+        } else {
+            chatController = new ChatController(this, handler);
+            connectToDevice(direccion_destino);
+        }
+    }
+
+    private Handler handler = new Handler(new Handler.Callback() {
+
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_STATE_CHANGE:  // Si es mensaje de cambio de estado
+                    switch (msg.arg1) {
+                        case ChatController.STATE_CONNECTED:  // Si es estado conectado
+                            setStatus("Conectado a: " + connectingDevice.getName());  // Se envía el mensaje como parámetro
+                            break;
+                        case ChatController.STATE_CONNECTING:  // Si es estado conectando
+                            setStatus("Conectando...");  // Se envía el mensaje como parámetro
+                            break;
+                        case ChatController.STATE_LISTEN:
+                            //setStatus("Escuchando");
+                        case ChatController.STATE_NONE:
+                            setStatus("No conectado");
+                            break;
+                    }
+                    break;
+                case MESSAGE_WRITE:    // Si es mensaje escrito
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    String writeMessage = new String(writeBuf);  // Se almacena el mensaje a mostrar
+                    Toast.makeText(ChatActivity.this, "Yo: " + writeMessage, Toast.LENGTH_SHORT).show();
+                    break;
+                case MESSAGE_READ:      // Si es mensaje lectura
+                    byte[] readBuf = (byte[]) msg.obj;
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    Toast.makeText(ChatActivity.this, connectingDevice.getName() + ":  " + readMessage,
+                            Toast.LENGTH_SHORT).show();
+                    break;
+
+                case MESSAGE_DEVICE_OBJECT:     // Si es mensaje del objeto del dispositivo
+                    connectingDevice = msg.getData().getParcelable(DEVICE_OBJECT);      // Se guarda los datos del dispositivo
+                    Toast.makeText(getApplicationContext(), "Conectado a " + connectingDevice.getName(),
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MESSAGE_TOAST:
+                    Toast.makeText(getApplicationContext(), msg.getData().getString("toast"),
+                            Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return false;
+        }
+    });
+
+
+    private void setStatus(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+    private void connectToDevice(String MAC) {
+        if (bluetoothAdapter == null) {
+            Log.i("Error", "Bluetooth not supported");
+        } else {
+            BluetoothDevice dispositivo = bluetoothAdapter.getRemoteDevice(MAC);
+            Toast.makeText(this, MAC, Toast.LENGTH_SHORT).show();
+            chatController.connect(dispositivo);
+        }
+    }
+
+    private void sendChatMessage(String mensaje){
+    //private boolean sendChatMessage(){
+        /*chatArrayAdapter.add(new ChatMessage(lado, textoMensaje.getText().toString()));
+        textoMensaje.setText("");
         lado = !lado;
-        return true;
+        return true;*/
+        if (chatController.getState() != ChatController.STATE_CONNECTED) {
+            Toast.makeText(this, "¡Connexión perdida!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (mensaje.length() > 0) {
+            byte[] send = mensaje.getBytes();
+            chatController.write(send, "texto");
+            textoMensaje.setText("");
+        }
     }
 
     // Personalizacción de actividad
@@ -89,9 +199,9 @@ public class ChatActivity extends AppCompatActivity {
     }
 
     public void findByIds(){
-        buttonSend = (Button) findViewById(R.id.btnSend);
+        botonEnviar = (Button) findViewById(R.id.btnSend);
         listView = (ListView) findViewById(R.id.listViewMsg);
-        chatText = (EditText) findViewById(R.id.txtMsg);
+        textoMensaje = (EditText) findViewById(R.id.txtMsg);
     }
 
 }
