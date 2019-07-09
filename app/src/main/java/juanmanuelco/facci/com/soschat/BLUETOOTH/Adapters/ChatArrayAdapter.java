d4e73d7f021419b9e05b691b5e825784e6b74059
Pablo España <pablo_cesar1995@hotmail.com>
2019-07-08 21:47:47 -0500

    Adaptador para imagen y texto

--- app/src/main/java/juanmanuelco/facci/com/soschat/BLUETOOTH/Adapters/ChatArrayAdapter.java
+++ app/src/main/java/juanmanuelco/facci/com/soschat/BLUETOOTH/Adapters/ChatArrayAdapter.java
@@ -1,12 +1,17 @@
 package juanmanuelco.facci.com.soschat.BLUETOOTH.Adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,8 +20,11 @@ import juanmanuelco.facci.com.soschat.R;
 
 public class ChatArrayAdapter extends ArrayAdapter<ChatMessage> {
     private TextView chatText;
+    private ImageView imagen;
     private List<ChatMessage> chatMessageList = new ArrayList<ChatMessage>();
     private Context context;
+    private Bitmap bitmap;
+
 
     @Override
     public void add(ChatMessage object) {
@@ -41,13 +49,32 @@ public class ChatArrayAdapter extends ArrayAdapter<ChatMessage> {
         ChatMessage chatMessageObj = getItem(position);
         View row = convertView;
         LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        if (chatMessageObj.left) {
-            row = inflater.inflate(R.layout.bt_element_right_msg, parent, false);
-        }else{
-            row = inflater.inflate(R.layout.bt_element_left_msg, parent, false);
+        if (chatMessageObj.tipo_contenido.equals("texto")){
+            if (chatMessageObj.left) {
+                row = inflater.inflate(R.layout.bt_element_right_msg, parent, false);
+            }else{
+                row = inflater.inflate(R.layout.bt_element_left_msg, parent, false);
+            }
+            chatText = (TextView) row.findViewById(R.id.msgr);
+            chatText.setText(chatMessageObj.message);
+        }else if (chatMessageObj.tipo_contenido.equals("imagen")){
+            if (chatMessageObj.left) {
+                row = inflater.inflate(R.layout.bt_element_right_img_msg, parent, false);
+            }else{
+                row = inflater.inflate(R.layout.bt_element_left_img_msg, parent, false);
+            }
+            try {
+                imagen = (ImageView) row.findViewById(R.id.imgSelect);
+                Uri imgUri = Uri.parse(chatMessageObj.message);
+                bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), imgUri);
+                //bitmap = Bitmap.createScaledBitmap(bitmap,  600 ,600, true);
+                imagen.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
-        chatText = (TextView) row.findViewById(R.id.msgr);
-        chatText.setText(chatMessageObj.message);
+
         return row;
     }
 }
