ddf704a616bcce959990d17a79b136593d217954
Pablo España <pablo_cesar1995@hotmail.com>
2019-07-08 21:48:23 -0500

    nuevo parametro agregado

--- app/src/main/java/juanmanuelco/facci/com/soschat/BLUETOOTH/Entities/ChatMessage.java
+++ app/src/main/java/juanmanuelco/facci/com/soschat/BLUETOOTH/Entities/ChatMessage.java
@@ -2,11 +2,12 @@ package juanmanuelco.facci.com.soschat.BLUETOOTH.Entities;
 
 public class ChatMessage {
     public boolean left;
-    public String message;
+    public String message, tipo_contenido;
 
-    public ChatMessage(boolean left, String message) {
+    public ChatMessage(boolean left, String message, String tipo_contenido) {
         super();
         this.left = left;
         this.message = message;
+        this.tipo_contenido = tipo_contenido;
     }
 }
